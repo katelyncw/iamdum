e08e7ea32dfa6f014bd580773c9fa1a94f5f942f
Katelyn <katelyncwong@gmail.com>
2020-09-05 12:27:42 -0700

    Initial Commit

---
+++ iamdum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iamdum
+//
+//  Created by Katelyn Wong on 9/5/20.
+//  Copyright © 2020 Katelyn Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
